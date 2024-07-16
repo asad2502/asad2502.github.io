97b2b485b88c04789c313b63ff74eeae05f08e41
asad2502 <cheesymeatballsoup@gmail.com>
2024-07-16 19:24:57 +0700

    trying new website

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Footer = (props) => {
+  const bgStyle = { backgroundColor: "#f5f5f5" };
+
+  return (
+    <footer style={bgStyle} className="mt-auto py-5 text-center ">
+      <Container>
+        {props.children}
+        Made with <i className="fas fa-heart" /> using <i className="fab fa-react" />{" "}!
+        <p>
+          <small className="text-muted">
+            This site is based on the open source template made by{" "}
+            <a
+            className="badge badge-dark"
+            rel="noopener"
+            href="https://github.com/hashirshoaeb"
+            aria-label="My GitHub"
+            >
+            Hashir Shoaib
+            </a>
+            {" "}. Feel free to fork and make your own
+            version.
+          </small>
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
